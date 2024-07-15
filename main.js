15b190da6ef0a6d1f96bb3f5c7c2a526304e7747
HadiZD01 <zidanihadi259@gmail.com>
2024-07-15 23:00:15 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-let insta = document.getElementById('insta');
-let fb = document.getElementById('fb');
-let disc = document.getElementById('disc');
+let btn = document.getElementById('menu');
+let nav =document.querySelector('.navbar');
+let menu =document.querySelector('.menu');
+let btnclose = document.querySelector('.close');
 
-function opacfb() {
-    fb.classList.add('opfb');
+btn.onclick = function() {
+    nav.classList.remove('menuhide');
+    menu.classList.add('menuhide');
+    btnclose.classList.remove('menuhide');
+}
+btnclose.onclick = function(){
+    nav.classList.add('menuhide');
+    menu.classList.remove('menuhide');
+    btnclose.classList.add('menuhide');
 }
 
-function opacinsta() {
-    insta.classList.add('opinsta');
-}
-
-function opacdisc() {
-    disc.classList.add('opdisc');
-}
